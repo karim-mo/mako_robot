0c58349faa7a0c1c1ffdc3666d76aac1d8195b39
Abdelrahman Tarek <abdelrahman_tarek_aast@hotmail.com>
2021-04-21 15:27:57 +0200

    updating the lidar node Discover the ranges of the rplidar and split Forward,Left,Right,Backward into ranges and print it

--- mako_ws/src/dev_test_tools/dev_test_tools/lidar_test.py
+++ mako_ws/src/dev_test_tools/dev_test_tools/lidar_test.py
@@ -17,6 +17,32 @@ class lidarTestNode(Node):
               str(msg.angle_max) + "  " + str(math.ceil((msg.angle_max - msg.angle_min) / msg.angle_increment)) 
               + "  " + str(len(msg.ranges)) + "  " + str(msg.angle_increment))
         time.sleep(3)
+        x = [0] *len(msg.ranges)
+        for i in range(len(msg.ranges)):
+            x[i] = msg.ranges[i]*100
+
+        r = len(msg.ranges)
+        #printing the reading of the forward, left, right and backward in cm
+        print("forward")
+        print("----------------------------------")
+        print("----------------------------------\n\n")
+        print(x[r-20:r])
+        print(x[0:20])
+        print("\n\n")
+        print("left")
+        print("----------------------------------")
+        print("----------------------------------\n\n")
+        print(x[int(r/4)-25:int(r/4)+25])
+        print("\n\n")
+        print("right")
+        print("----------------------------------")
+        print("----------------------------------\n\n")
+        print(x[int(3/4*r)-25:int(3/4*r)+25])
+        print("backward")
+        print("----------------------------------")
+        print("----------------------------------\n\n")
+        print(x[int(r/2)-25:int(r/2)+25])
+    
 
 
 def main(args=None):
